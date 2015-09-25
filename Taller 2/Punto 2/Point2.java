23ed63830edfb6bb7653b9b9e6946f3df3bd2105
jaeramirezga <jaeramirezga@gmail.com>
2015-09-25 17:53:18 -0500

    main comentado

--- Taller 2/Punto 2/Point2.java
+++ Taller 2/Punto 2/Point2.java
@@ -1,63 +1,112 @@
 public class Point2 {
+	// Crearemos una clase de con la cual haremos nuestro main
 	
 	public static void main(String[] args) {
 		
 		Rectangulo a = new Rectangulo();
+		// Crearemos un objeto llamado a, en el cual dejaremos los valores por defecto
 		System.out.println("El Area 1 es: "+ a.area() );
+		// Imprimiremos el valor del area
 		System.out.println("El perimetro 1 es: "+ a.perimetro() + "\n");
+		// Imprimiremos el valor del perimetro 
 		
 		Rectangulo b = new Rectangulo();
+		// Crearemos un objeto llamado a, en el cual dejaremos los valores por defecto
 		b.setLargo(13);
+		// Le daremos un valor de Largo
 		b.setAncho(9);
+		// Le daremos un valor de Ancho
 		System.out.println("El Area 2 es: "+ b.area() );
+		// Imprimiremos el valor del area
 		System.out.println("El perimetro 2 es: "+ b.perimetro() + "\n");
+		// Imprimiremos el valor del perimetro
 		
 		Rectangulo c = new Rectangulo();
+		// Crearemos un objeto llamado a, en el cual dejaremos los valores por defecto
 		c.setLargo(17);
+		// Le daremos un valor de Largo
 		c.setAncho(3);
+		// Le daremos un valor de Ancho
 		System.out.println("El Area 3 es: "+ c.area() );
+		// Imprimiremos el valor del area
 		System.out.println("El perimetro 3 es: "+ c.perimetro() + "\n");
+		// Imprimiremos el valor del perimetro
 		
 		Rectangulo d = new Rectangulo();
+		// Crearemos un objeto llamado a, en el cual dejaremos los valores por defecto
 		d.setLargo(1);
+		// Le daremos un valor de Largo
 		d.setAncho(10);
+		// Le daremos un valor de Ancho
 		System.out.println("El Area 4 es: "+ d.area() );
+		// Imprimiremos el valor del area
 		System.out.println("El perimetro 4 es: "+ d.perimetro() + "\n");
+		// Imprimiremos el valor del perimetro
 		
 		Rectangulo e = new Rectangulo();
+		// Crearemos un objeto llamado a, en el cual dejaremos los valores por defecto
 		e.setLargo(12);
+		// Le daremos un valor de Largo
 		e.setAncho(15);
+		// Le daremos un valor de Ancho
 		System.out.println("El Area 5 es: "+ e.area() );
+		// Imprimiremos el valor del area
 		System.out.println("El perimetro 5 es: "+ e.perimetro() + "\n");
+		// Imprimiremos el valor del perimetro
 		
 		Rectangulo f = new Rectangulo();
+		// Crearemos un objeto llamado a, en el cual dejaremos los valores por defecto
 		f.setLargo(2);
+		// Le daremos un valor de Largo
 		f.setAncho(18);
+		// Le daremos un valor de Ancho
 		System.out.println("El Area 6 es: "+ f.area() );
+		// Imprimiremos el valor del area
 		System.out.println("El perimetro 6 es: "+ f.perimetro() + "\n");
+		// Imprimiremos el valor del perimetro
 		
 		Rectangulo g = new Rectangulo();
+		// Crearemos un objeto llamado a, en el cual dejaremos los valores por defecto
 		g.setLargo(16);
+		// Le daremos un valor de Largo
 		g.setAncho(15);
+		// Le daremos un valor de Ancho
 		System.out.println("El Area 7 es: "+ g.area() );
+		// Imprimiremos el valor del area
 		System.out.println("El perimetro 7 es: "+ g.perimetro() + "\n");
+		// Imprimiremos el valor del perimetro
 		
 		Rectangulo h = new Rectangulo();
+		// Crearemos un objeto llamado a, en el cual dejaremos los valores por defecto
 		h.setLargo(11);
+		// Le daremos un valor de Largo
 		h.setAncho(11);
+		// Le daremos un valor de Ancho
 		System.out.println("El Area 8 es: "+ h.area() );
+		// Imprimiremos el valor del area
 		System.out.println("El perimetro 8 es: "+ h.perimetro() + "\n");
+		// Imprimiremos el valor del perimetro
 		
 		Rectangulo i = new Rectangulo();
+		// Crearemos un objeto llamado a, en el cual dejaremos los valores por defecto
 		i.setLargo(7);
+		// Le daremos un valor de Largo
 		i.setAncho(14);
+		// Le daremos un valor de Ancho
 		System.out.println("El Area 9 es: "+ i.area() );
+		// Imprimiremos el valor del area
 		System.out.println("El perimetro 9 es: "+ i.perimetro() + "\n");
+		// Imprimiremos el valor del perimetro
 		
 		Rectangulo j = new Rectangulo();
+		// Crearemos un objeto llamado a, en el cual dejaremos los valores por defecto
 		j.setLargo(18);
+		// Le daremos un valor de Largo
 		j.setAncho(3);
+		// Le daremos un valor de Ancho
 		System.out.println("El Area 10 es: "+ j.area() );
+		// Imprimiremos el valor del area
 		System.out.println("El perimetro 10 es: "+ j.perimetro() + "\n");
+		// Imprimiremos el valor del perimetro
 	}
 }
