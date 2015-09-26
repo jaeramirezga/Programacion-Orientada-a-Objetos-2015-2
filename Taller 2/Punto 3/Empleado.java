4e1b7c7223f40011e23fe14eb5b68a2d6eeb225d
jaeramirezga <jaeramirezga@gmail.com>
2015-09-26 11:50:22 -0500

    Archivo docx

--- Taller 2/Punto 3/Empleado.java
+++ Taller 2/Punto 3/Empleado.java
@@ -1,50 +1,77 @@
 
 public class Empleado {
+	// Creamos una Clase llamada empleado
 	
 	private String nombre;
+	// Creamos una variable privada para el nombre del empleado
 	private String apellido;
+	// Creamos una variable privada para el apellido del empleado
 	private double salario;
+	// Creamos una variable privada para el salario del empleado
 	private int cont = 0;
+	// Creamos una variable privada para el conteo del año
 	
 	public Empleado ( String nombre , String apellido , double salario ){
+		// Crearemos un constructor , y recibira 3 tipos de datos
 		this.nombre = nombre;
+		// Le pasaremos los valores recibidos, a la variable de la clase
 		this.apellido = apellido;
+		// Le pasaremos los valores recibidos, a la variable de la clase
 		this.salario = salario;
+		// Le pasaremos los valores recibidos, a la variable de la clase
 	}
 	
 	public void setNombre(String xNombre){
+		// Creamos un metodo de tipo Set, con el cual podremos modificar los valores de nombre
 		nombre = xNombre;
+		// Le pasaremos los valores recibidos, a la variable de la clase
 	}
 	
 	public void setApellido(String xApellido){
+		// Creamos un metodo de tipo Set, con el cual podremos modificar los valores de apellido
 		apellido = xApellido;
+		// Le pasaremos los valores recibidos, a la variable de la clase
 	}
 	
 	public void setSalario(double xSalario){
+		// Creamos un metodo de tipo Set, con el cual podremos modificar los valores de salario
 		if( xSalario < 0){
+			// Pondremos un condicional, por si nos dan un valor negativo, coloque el valor a salario como 0
 			xSalario = 0;
 		}
 		salario = xSalario;
+		// Le pasaremos los valores recibidos, a la variable de la clase
 	}
 	
 	public String getNombre(){
+		// Creamos un metodo de tipo Get, con el cual podremos devolver los valores de nombre
 		return nombre;
+		// Retornaremos los valores de nombre
 	}
 	
 	public String getApellido(){
+		// Creamos un metodo de tipo Get, con el cual podremos devolver los valores de apellido
 		return apellido;
+		// Retornaremos los valores de nombre
 	}
 	
 	public void setCont(int xCont){
+		// Creamos un metodo de tipo Set, con el cual podremos modificar los valores de Cont
 		cont = xCont;
+		// Le pasaremos los valores recibidos, a la variable de la clase
 	}
 	
 	public double Salario(){
+		// Crearemos un metodo publico en el cual debolveremos el valor del salario
 		if (cont == 1){
+			// Crearemos un condicional en el cual miraremos si cont es igual a uno
 			return salario;
+			// Retornaremos los valores de salario original si cont es 1
 		}
 		salario = (salario + salario*0.10);
+	    // Al valor de salario lo modificaremos, sumandole el 10% por cada año
 		return salario;
+		// Retornaremos los valores del salario
 	}
 	
 }
--- Taller 2/Punto 3/EmpleadoTest.java
+++ Taller 2/Punto 3/EmpleadoTest.java
@@ -1,29 +1,49 @@
 
 
 public class EmpleadoTest {
+	// Crearemos una clase llamada EmpleadoTest
     
 	public static void main(String[] args) {
+		// Crearemos nuestro main
 		
 		Empleado No1 = new Empleado( "Juan" , "Ramirez" , 12300);
+		// Crearemos un objeto llamado No1, y le pasaremos unos valores con los cuales lo instanciaremos
 		Empleado No2 = new Empleado( "Daniela" , "Ramirez" , 5432);
+		// Crearemos un objeto llamado No2, y le pasaremos unos valores con los cuales lo instanciaremos
 		Empleado No3 = new Empleado( "Juan" , "Contreras" , 1000000);
+		// Crearemos un objeto llamado No3, y le pasaremos unos valores con los cuales lo instanciaremos
 		Empleado No4 = new Empleado( "Andres" , "Corson" , 5000000);
+		// Crearemos un objeto llamado No4, y le pasaremos unos valores con los cuales lo instanciaremos
 		Empleado No5 = new Empleado( "Augusto" , "Ramirez" , 3000000);
+		// Crearemos un objeto llamado No5, y le pasaremos unos valores con los cuales lo instanciaremos
 		
 		for (int i = 1 ; i < 5 ; i++){
+			// Crearemos un ciclo de tipo for, el cual utilizaremos para pasar los valores a Cont en la clase empleado
 			No1.setCont(i);
+			// Le pasaremos a No1 los valores de i, variable del for
 			No2.setCont(i);
+			// Le pasaremos a No2 los valores de i, variable del for
 			No3.setCont(i);
+			// Le pasaremos a No3 los valores de i, variable del for
 			No4.setCont(i);
+			// Le pasaremos a No4 los valores de i, variable del for
 			No5.setCont(i);
+			// Le pasaremos a No5 los valores de i, variable del for
 			
 			System.out.println("Año: " + i);
+			// Imprimiremos el numero del año
 			System.out.println("Empleado 1: "+ No1.getNombre() + " " + No1.getApellido() + ", Salario anual: " + No1.Salario() );
+			// Imprimimos el Empleado No1, con su nombre, su apellido y el valor del salario
 			System.out.println("Empleado 2: "+ No2.getNombre() + " " + No2.getApellido() + ", Salario anual: " + No2.Salario() );
+			// Imprimimos el Empleado No2, con su nombre, su apellido y el valor del salario
 			System.out.println("Empleado 3: "+ No3.getNombre() + " " + No3.getApellido() + ", Salario anual: " + No3.Salario() );
+			// Imprimimos el Empleado No3, con su nombre, su apellido y el valor del salario
 			System.out.println("Empleado 4: "+ No4.getNombre() + " " + No4.getApellido() + ", Salario anual: " + No4.Salario() );
+			// Imprimimos el Empleado No4, con su nombre, su apellido y el valor del salario
 			System.out.println("Empleado 5: "+ No5.getNombre() + " " + No5.getApellido() + ", Salario anual: " + No5.Salario() );
+			// Imprimimos el Empleado No5, con su nombre, su apellido y el valor del salario
 			System.out.println();
+			// Imprimiremos una linea en blanco
 		}
 	}
 
