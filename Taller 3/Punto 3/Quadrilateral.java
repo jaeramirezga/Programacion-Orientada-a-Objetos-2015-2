b1cca01db1965a95502421973414e321ef12e448
jaeramirezga <jaeramirezga@gmail.com>
2015-10-04 22:55:25 -0500

    Comentarios

--- Taller 3/Punto 3/Quadrilateral.java
+++ Taller 3/Punto 3/Quadrilateral.java
@@ -31,30 +31,18 @@ public class Quadrilateral {
 		// Le pasaremos los valores recibidos en nuestro constructor, al punto p4
 	}
 	
-	public double getDist1(Pair a, Pair b) {
+	public double getDist(Pair a, Pair b) {
 		// Crearemos un metodo de tipo get, con el cual recibiremos las coordenadas de dos puntos
 		return Math.sqrt((a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y));
 		// Retornaremos el valor de la distancia entre los dos puntos
 	}
 	
-	public double getDist2(Pair c, Pair d) {
+	public double getHeight(Pair a, Pair b){
 		// Crearemos un metodo de tipo get, con el cual recibiremos las coordenadas de dos puntos
-		return Math.sqrt((c.x - d.x) * (c.x - d.x) + (c.y - d.y) * (c.y - d.y));
-		// Retornaremos el valor de la distancia entre los dos puntos
-	}
-	
-	public double getHeight1(Pair a, Pair c){
-		// Crearemos un metodo de tipo get, con el cual recibiremos las coordenadas de dos puntos
-		return Math.sqrt((a.y - c.y)*(a.y - c.y));
+		return Math.sqrt((a.y - b.y)*(a.y - b.y));
 		// Retornaremos el valor de la altura
 	}
-	
-	public double getHeight2(Pair b, Pair d){
-		// Crearemos un metodo de tipo get, con el cual recibiremos las coordenadas de dos puntos
-		return Math.sqrt((b.y - d.y)*(b.y - d.y));
-		// Retornaremos el valor de la altura
-	}
-	
+		
 }
 
 
@@ -69,11 +57,11 @@ class Trapezoid extends Quadrilateral{
 	
 	public double area() {
 		// Crearemos un metodo llamado area
-		double width1 = getDist1(p1, p2);
+		double width1 = getDist(p1, p2);
 		// Crearemos una variable, con la cual sabremos la distancia de uno de los lados
-		double width2 = getDist2(p3, p4);
+		double width2 = getDist(p3, p4);
 		// Crearemos una variable, con la cual sabremos la distancia de uno de los lados
-		double height = getHeight1;
+		double height = getHeight(p1, p3);
 		// Crearemos una variable, con la cual sabremos la Altura de la figura
 		return (((width1)+(width2))*height)/2;
 		// Retornaremos el valor del area
@@ -92,9 +80,9 @@ class Parallelogram extends Quadrilateral{
 	
 	public double area() {
 		// Crearemos un metodo llamado area
-		double width1 = getDist1(p1, p2);
+		double width1 = getDist(p1, p2);
 		// Crearemos una variable, con la cual sabremos la distancia de uno de los lados
-		double height = getHeight1;
+		double height = getHeight(p1, p3);
 		// Crearemos una variable, con la cual sabremos la Altura de la figura
 		return ((width1)*height);
 		// Retornaremos el valor del area
@@ -113,9 +101,9 @@ class Rectangle extends Quadrilateral{
 	
 	public double area() {
 		// Crearemos un metodo llamado area
-		double width1 = getDist1(p1, p2);
+		double width1 = getDist(p1, p2);
 		// Crearemos una variable, con la cual sabremos la distancia de uno de los lados
-		double height = getHeight1;
+		double height = getHeight(p1, p3);
 		// Crearemos una variable, con la cual sabremos la Altura de la figura
 		return ((width1)*height);
 		// Retornaremos el valor del area
@@ -134,7 +122,7 @@ class Square extends Quadrilateral {
 	
 	public double area() {
 		// Crearemos un metodo llamado area
-		double side = getDist1(p1, p2);
+		double side = getDist(p1, p2);
 		// Crearemos una variable, con la cual sabremos la distancia de uno de los lados
 		return side * side;
 		// Retornaremos el valor del area
@@ -142,49 +130,3 @@ class Square extends Quadrilateral {
 
 }
 
-public static void main(String[] args) {
-	Scanner sc = new Scanner(System.in);
-	double[] puntosX1 = new double [5];
-	double[] puntosX2 = new double [5];
-	double[] puntosX3 = new double [5];
-	double[] puntosX4 = new double [5];
-	
-	double[] puntosY1 = new double [5];
-	double[] puntosY2 = new double [5];
-	double[] puntosY3 = new double [5];
-	double[] puntosY4 = new double [5];
-	
-	
-	for (int i = 0 ; i < 4 ; i++){
-		System.out.print("Digite el punto " + 1 + " en X: ");
-		puntosX1 [i] = Scanner.sc;
-		System.out.println();
-		System.out.print("Digite el punto " + 1 + " en Y: ");
-		puntosY1 [i] = Scanner.sc;
-		System.out.println();
-		System.out.print("Digite el punto " + 2 + " en X: ");
-		puntosX2 [i] = Scanner.sc;
-		System.out.println();
-		System.out.print("Digite el punto " + 2 + " en Y: ");
-		puntosY2 [i] = Scanner.sc;
-		System.out.println();
-		System.out.print("Digite el punto " + 3 + " en X: ");
-		puntosX3 [i] = Scanner.sc;
-		System.out.println();
-		System.out.print("Digite el punto " + 3 + " en Y: ");
-		puntosY3 [i] = Scanner.sc;
-		System.out.println();
-		System.out.print("Digite el punto " + 4 + " en X: ");
-		puntosX4 [i] = Scanner.sc;
-		System.out.println();
-		System.out.print("Digite el punto " + 4 + " en Y: ");
-		puntosY4 [i] = Scanner.sc;
-		System.out.println();
-	}
-	Figura1 = new Quadrilateral((puntosX1 [0], puntosY1 [0]),(puntosX2 [0],puntosY2 [0]),(puntosX3 [0],puntosY3 [0]),(puntosX4 [0],puntosY4 [0]));
-	Figura2 = new Trapezoid((puntosX1 [1], puntosY1 [1]),(puntosX2 [1],puntosY2 [1]),(puntosX3 [1],puntosY3 [1]),(puntosX4 [1],puntosY4 [1]));
-	Figura3 = new Parallelogram((puntosX1 [2], puntosY1 [2]),(puntosX2 [2],puntosY2 [2]),(puntosX3 [2],puntosY3 [2]),(puntosX4 [2],puntosY4 [2]));
-	Figura4 = new Rectangle((puntosX1 [3], puntosY1 [3]),(puntosX2 [3],puntosY2 [3]),(puntosX3 [3],puntosY3 [3]),(puntosX4 [3],puntosY4 [3]));
-	Figura5 = new Square((puntosX1 [4], puntosY1 [4]),(puntosX2 [4], puntosY2 [4]),(puntosX3 [4],puntosY3 [4]),(puntosX4 [4],puntosY4 [4]));
-	
-}
