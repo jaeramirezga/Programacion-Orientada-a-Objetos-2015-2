f43b60f86edec744702228252c5fd47a6c4784a8
Juan Felipe Contreras Correa <jufcontrerasco@unal.edu.co>
2015-10-25 13:47:48 -0500

    Final Version

--- Ajedrez/src/Main.java
+++ Ajedrez/src/Main.java
@@ -11,7 +11,7 @@ public class Main {
 
 	public static void main(String[] args) throws Exception {
 
-		System.out.println("Bienvenidos a Ajedrez");
+		System.out.println("Bienvenidos a Ajedrez Jubilado");
 		SelectorInicial();
 
 	}
