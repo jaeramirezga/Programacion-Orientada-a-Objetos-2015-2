0d11612c783d65a2e9a94bab36a6250650288407
jaeramirezga <jaeramirezga@gmail.com>
2015-10-27 22:46:17 -0500

    punto 4

---
+++ Taller 4/Taller4puntosdel3/Pair.java
@@ -0,0 +1,16 @@
+package Taller4puntosdel3;
+
+public class Pair {
+	//Se crea una clase llamada pair
+		double x, y;
+		//Crearemos unas variables de tipo double para poder guardar 
+		
+		public Pair(double x, double y) {
+			//Recibiremos un valor de tipo double x y y
+			this.x = x;
+			//Le pasaremos a nuestra variable x el valor recibido en nuestro constructor
+			this.y = y;
+			//Le pasaremos a nuestra variable y el valor recibido en nuestro constructor
+		}
+
+}
